9c4f38f677abe45dee5716d9cfdc7afdd66908a5
gagaspbahar <gagaspbahar@gmail.com>
2022-04-28 20:52:19 +0700

    Backend working

--- backend/main.go
+++ backend/main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"time"
 
 	"github.com/gagaspbahar/dna-pattern-matching-web/algo/add/stringmatching"
+	search_data "github.com/gagaspbahar/dna-pattern-matching-web/algo/search"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
 	username = "root"
-	password = ""
+	password = "password"
 	hostname = "127.0.0.1:3306"
-	dbname   = "tubes3"
+	dbname   = "tubes3_basdat"
 )
 
 type Penyakit struct {
@@ -31,6 +34,18 @@ type TesDNA struct {
 	SequenceDNA string `json:"sequenceDNA"`
 	Penyakit    string `json:"penyakit"`
 	Metode      string `json:"metode"`
+}
+
+type HistoryDNA struct {
+	Query string `json:"query"`
+}
+
+type Record struct {
+	Tanggal       string  `json:"tanggal"`
+	Nama_pengguna string  `json:"nama_pengguna"`
+	Nama_penyakit string  `json:"nama_penyakit"`
+	Similarity    float64 `json:"similarity"`
+	Status_tes    int     `json:"status_tes"`
 }
 
 func dsn() string {
@@ -46,16 +61,16 @@ func main() {
 		return
 	}
 	defer db.Close()
-	// search_data.Search_db(db)
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	// r.POST("/add", postAddPenyakit)
+	r.POST("/add", postAddPenyakit(db))
 	r.POST("/upload/:filename", handleUpload)
-	r.GET("/tes", getTesDNA)
+	r.GET("/tes", getTesDNA(db))
+	r.GET("/history", getDNAHistory(db))
 	r.Run()
 }
 
@@ -70,7 +85,7 @@ func handleUpload(c *gin.Context) {
 	filename := c.Param("filename")
 	filename = filename + ".txt"
 	fmt.Println(filename)
-	out, err := os.Create("/data/sequence" + filename)
+	out, err := os.Create("/data/sequence/" + filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -84,37 +99,139 @@ func handleUpload(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"filepath": filepath})
 }
 
-func getTesDNA(c *gin.Context) {
-	var tesDNA TesDNA
-	var result int
-	if err := c.BindJSON(&tesDNA); err != nil {
-		c.String(http.StatusBadRequest, "Bad request")
-		log.Fatal(err)
-		return
+func postAddPenyakit(db *sql.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var penyakit Penyakit
+		if err := c.BindJSON(&penyakit); err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			log.Fatal(err)
+			return
+		}
+		filename := penyakit.Penyakit
+		filepath := "/data/sequence/" + penyakit.Penyakit + ".txt"
+		sequence, err := os.Create(filepath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer sequence.Close()
+
+		content, err := os.ReadFile("data/sequence/" + filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		result := CheckDNASequence(string(content))
+		if result {
+			insert, err := db.Query("INSERT INTO sequence_penyakit VALUES ( ?, ? )", penyakit, content)
+			if err != nil {
+				log.Printf("Error %s when insert to DB\n", err)
+			}
+			defer insert.Close()
+			c.JSON(http.StatusOK, gin.H{"message": "success"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "failed"})
+		}
 	}
+}
 
-	filepath := "/data/sequenceUser/" + tesDNA.SequenceDNA + ".txt"
+func getTesDNA(db *sql.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var tesDNA TesDNA
+		var result int
+		var sim float64
+		if err := c.BindJSON(&tesDNA); err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			log.Fatal(err)
+			return
+		}
 
-	sequence, err := os.ReadFile(filepath)
-	if err != nil {
-		log.Fatal(err)
+		filepath := "data/sequenceUser/" + tesDNA.SequenceDNA + ".txt"
+
+		sequence, err := os.ReadFile(filepath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sequenceString := string(sequence)
+
+		filepath = "data/sequence/" + tesDNA.Penyakit + ".txt"
+		sequence_penyakit, err := os.ReadFile(filepath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		spString := string(sequence_penyakit)
+
+		if tesDNA.Metode == "KMP" {
+			result = stringmatching.Boyermoore(sequenceString, spString)
+		} else {
+			result = stringmatching.KMP(sequenceString, spString)
+		}
+
+		if result == 1 {
+			sim = 100
+		} else {
+			sim = float64(stringmatching.LCS(sequenceString, spString)) / float64(len(spString))
+			if sim >= 80 {
+				result = 1
+			}
+		}
+
+		tanggal := time.Now().Format("2006-01-02")
+
+		insert, err := db.Query("INSERT INTO data_uji (tanggal_tes, nama_pengguna, nama_penyakit, similarity, status_tes) VALUES ( ?, ? , ?, ?, ? )", tanggal, tesDNA.Nama, tesDNA.Penyakit, sim, result)
+		if err != nil {
+			log.Printf("Error %s when insert to DB\n", err)
+		}
+
+		defer insert.Close()
+
+		c.JSON(http.StatusOK, gin.H{"result": result, "nama": tesDNA.Nama, "penyakit": tesDNA.Penyakit, "tanggal": tanggal, "similarity": sim})
 	}
-	sequenceString := string(sequence)
+}
 
-	filepath = "/data/sequence/" + tesDNA.Penyakit + ".txt"
-	sequence_penyakit, err := os.ReadFile(filepath)
-	if err != nil {
-		log.Fatal(err)
+func getDNAHistory(db *sql.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var results *sql.Rows
+		var err error
+		var history HistoryDNA
+		var records []Record
+
+		if err := c.BindJSON(&history); err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			log.Fatal(err)
+			return
+		}
+
+		data := history.Query
+		date, penyakit := search_data.CheckSequence(data)
+		date, penyakit = search_data.Clear_whitespace(date, penyakit)
+		if date != "" && penyakit != "" {
+			results, err = db.Query("SELECT tanggal_tes, nama_pengguna, nama_penyakit, similarity, status_tes FROM data_uji WHERE tanggal_tes = ? AND nama_penyakit = ?", date, penyakit)
+		} else if penyakit != "" {
+			results, err = db.Query("SELECT tanggal_tes, nama_pengguna, nama_penyakit, similarity, status_tes FROM data_uji WHERE nama_penyakit = ?", penyakit)
+		} else if date != "" {
+			results, err = db.Query("SELECT tanggal_tes, nama_pengguna, nama_penyakit, similarity, status_tes FROM data_uji WHERE tanggal_tes = ?", date)
+		} else {
+			return
+		}
+		if err != nil {
+			log.Printf("Error %s when SELECT from DB\n", err)
+		}
+		for results.Next() {
+			var record Record
+			// for each row, scan the result into our tag composite object
+			err = results.Scan(&record.Tanggal, &record.Nama_pengguna, &record.Nama_penyakit, &record.Similarity, &record.Status_tes)
+			if err != nil {
+				log.Printf("Error %s when insert to DB\n", err)
+			}
+			records = append(records, record)
+		}
+		recordsJson, _ := json.Marshal(records)
+		fmt.Println(records)
+		fmt.Println(string(recordsJson))
+		c.JSON(http.StatusOK, gin.H{"records": string(recordsJson)})
 	}
-	spString := string(sequence_penyakit)
+}
 
-	if tesDNA.Metode == "KMP" {
-		result = stringmatching.Boyermoore(sequenceString, spString)
-	} else {
-		result = stringmatching.KMP(sequenceString, spString)
-	}
-
-	tanggal := time.Now().Format("2006-01-02")
-
-	c.JSON(http.StatusOK, gin.H{"result": result, "nama": tesDNA.Nama, "penyakit": tesDNA.Penyakit, "tanggal": tanggal})
+func CheckDNASequence(input string) bool {
+	res1, _ := regexp.MatchString(`^[CAGT]+$`, input)
+	return res1
 }
